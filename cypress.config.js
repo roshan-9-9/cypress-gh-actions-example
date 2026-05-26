06979432eafe87ff78085d53c5fa8a76a6d62dcb
roshan-91 <rpatne4@gmail.com>
2026-05-26 14:55:55 +0530

    add reporter generator

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+  reporter: "mochawesome",
+  reporterOptions: {
+    overwrite: false,
+    html: false,
+    json: true,
   },
 });
